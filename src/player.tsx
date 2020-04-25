d4b446d9a9804db05a67b979c3d915c161cc88c9
khoidd <khoidd@vng.com.vn>
2020-04-25 12:21:11 +0700

    add album art component

--- src/player.tsx
+++ src/player.tsx
@@ -1,7 +1,7 @@
 import React, {FC} from 'react';
 import {StatusBar, View} from 'react-native';
+import AlbumArt from './album-art';
 import Header from './header';
-// import AlbumArt from './AlbumArt';
 // import TrackDetails from './TrackDetails';
 // import SeekBar from './SeekBar';
 // import Controls from './Controls';
@@ -11,8 +11,8 @@ const Player: FC = () => {
     <View style={styles.container}>
       <StatusBar hidden />
       <Header message="Album nhạc buồn thất tình" />
-      {/* <AlbumArt url='https://i.ytimg.com/vi/-EGVaRBv2e0/maxresdefault.jpg' />
-                <TrackDetails title={'Ký ức ngủ quên'} artist={'BÍCH PHƯƠNG'} />
+      <AlbumArt url="https://photo-resize-zmp3.zadn.vn/w480_r1x1_jpeg/cover/b/0/1/c/b01cef9849731963d490c938fe4bbf7f.jpg" />
+      {/* <TrackDetails title={'Ký ức ngủ quên'} artist={'BÍCH PHƯƠNG'} />
                 <SeekBar />
                 <Controls
                     repeatOn
