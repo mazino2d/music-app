5638aab475d763075ee047fd39ba78e68d880067
khoidd <khoidd@vng.com.vn>
2020-05-07 21:50:41 +0700

    add login page

--- src/navigator/stack.tsx
+++ src/navigator/stack.tsx
@@ -1,23 +1,29 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import React, {FC} from 'react';
 import Home from '../page/home';
-import Lyric from '../page/lyric';
+import Login from '../page/login';
+import LyricPlayer from '../page/lyric';
 import Player from '../page/player';
 
 const Stack = createStackNavigator();
 
 const StackNavigator: FC = () => (
   <Stack.Navigator
-    initialRouteName="Home"
+    initialRouteName="Login"
     headerMode="float"
     screenOptions={{
       headerTitleAlign: 'center',
       headerStatusBarHeight: 0,
       headerStyle: {backgroundColor: 'black'},
     }}>
+    <Stack.Screen
+      name="Login"
+      component={Login}
+      options={{headerShown: false}}
+    />
     <Stack.Screen name="Home" component={Home} />
     <Stack.Screen name="Player" component={Player} />
-    <Stack.Screen name="Lyric" component={Lyric} />
+    <Stack.Screen name="Lyric" component={LyricPlayer} />
   </Stack.Navigator>
 );
 
